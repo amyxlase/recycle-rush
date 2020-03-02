685e57abce156d949668f5099cff8f0469ebb45a
uwu <uwu>
2020-03-01 16:39:19 -0800

    Initial Commit

---
+++ Recycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recycle
+//
+//  Created by Amy Lin on 3/1/20.
+//  Copyright © 2020 uwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
